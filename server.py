527b241dc55aab6b89aa04658392a82d4cb7afb1
misstong <1962788480@qq.com>
2019-11-18 22:16:30 +0800

    echo request info

--- server.py
+++ server.py
@@ -1,14 +1,41 @@
 import http.server
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
-	page = "hello"
+	page = """
+	<html>
+	<body>
+	<table>
+	<tr> <td>Header</td> 		<td>Value</td> 		  </tr>
+	<tr> <td>Date and time</td> <td>{date_time}</td>  </tr>
+	<tr> <td>Client host</td>	<td>{client_host}</td></tr>
+	<tr> <td>Client port</td>	<td>{client_port}</td></tr>
+	<tr> <td>Command	</td>	<td>{command}</td>	  </tr>
+	<tr> <td>Path		</td>   <td>{path}</td>		  </tr>
+	</table>
+	</body>
+	</html>
+	"""
 
 	def do_GET(self):
+		page = self.create_page()
+		self.send_page(page)
+
+	def create_page(self):
+		values = {
+			'date_time':self.date_time_string(),
+			'client_host':self.client_address[0],
+			'client_port':self.client_address[1],
+			'command':self.command,
+			'path':self.path
+		}
+		return self.page.format(**values)
+
+	def send_page(self,page):
 		self.send_response(200)
 		self.send_header("Content-type","text/html")
-		self.send_header("Content-Length",str(len(self.page)))
+		self.send_header("Content-Length",str(len(page)))
 		self.end_headers()
-		self.wfile.write(self.page.encode())
+		self.wfile.write(page.encode())
 
 	
 if __name__ == '__main__':
