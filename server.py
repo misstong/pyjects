b304cb75708afacf4a85b904c294912a9cdb5193
misstong <1962788480@qq.com>
2019-11-18 22:43:34 +0800

    static server

--- server.py
+++ server.py
@@ -1,24 +1,47 @@
 import http.server
+import os
 
+
+class ServerException:
+	pass
 class RequestHandler(http.server.BaseHTTPRequestHandler):
-	page = """
+	Error_page = """
 	<html>
 	<body>
-	<table>
-	<tr> <td>Header</td> 		<td>Value</td> 		  </tr>
-	<tr> <td>Date and time</td> <td>{date_time}</td>  </tr>
-	<tr> <td>Client host</td>	<td>{client_host}</td></tr>
-	<tr> <td>Client port</td>	<td>{client_port}</td></tr>
-	<tr> <td>Command	</td>	<td>{command}</td>	  </tr>
-	<tr> <td>Path		</td>   <td>{path}</td>		  </tr>
-	</table>
+	<h1>Error accessing {path}</h1>
+	<p>{msg}</p>
 	</body>
 	</html>
 	"""
 
 	def do_GET(self):
-		page = self.create_page()
-		self.send_page(page)
+		try:
+			full_path = os.getcwd() + self.path
+			print(full_path)
+
+			if not os.path.exists(full_path):
+				raise ServerException("'%s' not found" %self.path)
+
+			elif os.path.isfile(full_path):
+				self.handle_file(full_path)
+			else:
+				raise ServerException("Unknown object '%s" % self.path)
+
+		except Exception as msg:
+			self.handle_error(msg)
+
+	def handle_file(self,full_path):
+		try:
+			with open(full_path,'r') as reader:
+				content = reader.read()
+			self.send_page(content)
+		except IOError as msg:
+			msg = "'%s' cannot be read: %s" %(self.path,msg)
+			self.handle_error(msg)
+
+	def handle_error(self,msg):
+		content = self.Error_page.format(path=self.path,msg=msg)
+		self.send_page(content,404)
 
 	def create_page(self):
 		values = {
@@ -30,8 +53,8 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
 		}
 		return self.page.format(**values)
 
-	def send_page(self,page):
-		self.send_response(200)
+	def send_page(self,page,status=200):
+		self.send_response(status)
 		self.send_header("Content-type","text/html")
 		self.send_header("Content-Length",str(len(page)))
 		self.end_headers()
